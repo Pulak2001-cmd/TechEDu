1b6641531652a7f843c362dedabf7b6051454fc4
Pulak2001-cmd <81929404+Pulak2001-cmd@users.noreply.github.com>
2021-09-08 23:11:56 +0530

    Add files via upload

---
+++ coursedetails1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+  </head>
+  <body>
+
+    <div class="card middle">
+      <div class="front">
+        <img src="img.jpg" alt="">
+      </div>
+      <div class="back">
+        <div class="back-content middle">
+          <h2>Heading</h2>
+          <br><br>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquam animi similique optio excepturi natus reiciendis quasi libero, porro perferendis.</span>
+          <br><br>
+          <div class="sm">
+            <a href="">Enroll Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
